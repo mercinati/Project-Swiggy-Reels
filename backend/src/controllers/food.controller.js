63231a59284f98a29a0362db7de3dd7cada7f6cb
karan gupta <karangupta9755@gmail.com>
2025-12-05 01:32:49 +0530

    get food item function created

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -20,7 +20,15 @@ async function createFood(req, res) {
     })
 }
 
-async function getFoodItems(req, res) {}
+async function getFoodItems(req, res) {
+
+    const foodItems = await foodModel.find({ })
+    
+    res.status(200).json({
+        message: "Food items fetched successfully",
+        food: foodItems
+    })
+}
 
 module.exports = {
     createFood,
