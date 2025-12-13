0aba30b4b241316684a5db8d62fcec9905c2b864
karan gupta <karangupta9755@gmail.com>
2025-12-14 02:00:07 +0530

    Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/',
+export default defineConfig(({ mode }) =>{
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/',
+  }
 })
